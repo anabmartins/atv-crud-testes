eae73e6ac740af9127a3b8c03ed54d31ac2cb0eb
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2023-03-13 08:47:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 function verify(){
         let email = document.getElementById('email').value
        // let psw = document.getElementById('psw').value
-        // usuario = email.substring(0, email.indexOf('@'))
-
+        usuario = email.substring(0, email.indexOf('@'))
+        dominio = email.substring(email.indexOf('@')+1, email.length)
         if(email.length === 0){
                 document.getElementById('msgemail').innerHTML = "<font color='red'>tem que colocar email ai</font>"                
         } else if(
-              (usuario.length>=1)
+              (usuario.length >= 1)&&
+              (dominio.length >= 3)&&
+              (dominio.search('@') ==-1) &&
+              (usuario.search('@') ==-1)
         )
-
         {
-               
-         }
+
+        }
 }
