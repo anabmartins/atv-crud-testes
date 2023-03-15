02f65b4235532d823ca6f56cea025104f00097ab
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2023-03-14 22:09:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-function verify(){
-       let email = document.getElementById('email').value
-       let psw = document.getElementById('psw').value
-        usuario = email.substring(0, email.indexOf('@'))
-        dominio = email.substring(email.indexOf('@')+1, email.length)
-        if(email.length === 0){
-                document.getElementById('msgemail').innerHTML = "<font color='red'>tem que colocar email ai</font>"                
-        } else if(
-              (usuario.length >= 1)&&
-              (dominio.length >= 3)&&
-              (dominio.search('@') ==-1) &&
-              (usuario.search('@') ==-1)
-        )
-        {
-                window.location.href = './cadastro.html'
-        }
+function verify() {
+  let email = document.getElementById("email").value;
+  let psw = document.getElementById("psw").value;
+  let usuario = email.substring(0, email.indexOf("@"));
+  let dominio = email.substring(email.indexOf("@") + 1, email.length);
+  if (email.length === 0) {
+    document.getElementById("msgemail").innerHTML =
+      "<font color='red'>Informe seu e-mail</font>";
+  } else if (
+    usuario.length >= 1 &&
+    dominio.length >= 3 &&
+    dominio.search("@") == -1 &&
+    usuario.search("@") == -1
+  ) {
+    if (psw.length === 0 || psw.length <= 3) {
+      document.getElementById("msgsenha").innerHTML =
+        "<font color='red'>Senha inválida</font>";
+    } else {
+      window.location.href = "./cadastro.html";
+    }
+  } else {
+    document.getElementById("msgemail").innerHTML =
+      "<font color='red'>E-mail inválido</font>";
+  }
 }
